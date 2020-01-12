c7eec808078d1a2b11ce425add51c0633c7f19a6
Nishad Aherrao <nishad.aherrao10@gmail.com>
2020-01-12 00:09:45 -0600

    Minor changes for bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = () => {
   return [
@@ -9,7 +10,7 @@ module.exports = () => {
       devtool: 'source-map',
       stats: { modules: false },
       entry: { main: './ClientApp/boot.tsx' },
-      resolve: { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+      resolve: { extensions: ['*', '.js', '.jsx', '.ts', '.tsx'] },
       output: {
         path: path.join(__dirname, 'dist'),
         filename: 'bundle.min.js',
@@ -26,7 +27,10 @@ module.exports = () => {
             test: /\.css$/,
             use: ['style-loader', 'css-loader']
           },
-          { test: /\.(png|jpg|jpeg|gif|svg)$/, use: 'url-loader?limit=25000' }
+          {
+            test: /\.(png|svg|jpg|gif)$/,
+            use: ['file-loader']
+          }
         ]
       },
       plugins: [
